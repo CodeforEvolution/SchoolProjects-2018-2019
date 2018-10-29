c7a8664beb851abe68014b8fe93eea6bb49356d8
CodeforEvolution <secundaja@gmail.com>
2018-10-29 13:39:37 -0500

    More Prob 5

--- Chapter7Stuff-ArrayReview/src/Hexagon.java
+++ Chapter7Stuff-ArrayReview/src/Hexagon.java
@@ -1,56 +1,93 @@
+import java.awt.Color;
 import java.awt.Point;
 
 
 public class Hexagon extends MoverPainter {
 	Canvas drawPlace;
-	boolean concave;
+	boolean convex;
+	int scale;
 	
-	final int HEX_SIZE = 10;
+	final int HEX_SIZE = 6;
 
-	public Hexagon(Canvas surface, Point center, boolean isConcave)
+	public Hexagon(Canvas surface, Point center, int theSize, boolean isConvex)
 	{
 		drawPlace = surface;
 		currentPos = center;
-		concave = isConcave;
+		convex = isConvex;
+		scale = theSize;
+		
+		draw(currentPos);
 	}
 	
 	protected void draw(Point where)
 	{
-		drawPlace.drawFilledPolygon(createXs(), createYs(), HEX_SIZE);
+		drawPlace.drawFilledPolygon(createXs(where), createYs(where), HEX_SIZE);
 	}
 	
 	protected void erase(Point where)
 	{
+		Color old = drawPlace.getInkColor();
 		
+		drawPlace.setInkColor(drawPlace.getBackgroundColor());
+		drawPlace.drawFilledPolygon(createXs(where), createYs(where), HEX_SIZE);
+		
+		drawPlace.setInkColor(old);
 	}
 	
-	private int[] createXs()
+	private int[] createXs(Point where)
 	{
 		int xPoints[] = new int[6];
 		
-		if (concave == true)
+		if (convex == true)
 		{
-			
+			xPoints[0] = where.x - (2 * scale);
+			xPoints[1] = where.x - (1 * scale);
+			xPoints[2] = where.x + (1 * scale);
+			xPoints[3] = where.x + (2 * scale);
+			xPoints[4] = where.x + (1 * scale);
+			xPoints[5] = where.x - (1 * scale);
 		}
 		else
 		{
-			
+			xPoints[0] = where.x - (2 * scale);
+			xPoints[1] = where.x - (1 * scale);
+			xPoints[2] = where.x + (1 * scale);
+			xPoints[3] = where.x + (2 * scale);
+			xPoints[4] = where.x + (1 * scale);
+			xPoints[5] = where.x - (1 * scale);
+		}
+		
+		for (int i = 0; i < 6; i++)
+		{
+			xPoints[i] *= scale;
 		}
 		
 		return xPoints;
 	}
 	
-	private int[] createYs()
+	private int[] createYs(Point where)
 	{
 		int yPoints[] = new int[6];
 		
-		if (concave == true)
+		int centerOff = 5 * scale;
+		
+		if (convex == true)
 		{
-			
+			yPoints[0] = where.y;
+			yPoints[1] = where.y - centerOff;
+			yPoints[2] = where.y - centerOff;
+			yPoints[3] = where.y;
+			yPoints[4] = where.y + centerOff;
+			yPoints[5] = where.y + centerOff;
 		}
 		else
 		{
-			
+			yPoints[0] = where.y;
+			yPoints[1] = where.y - centerOff;
+			yPoints[2] = where.y - centerOff;
+			yPoints[3] = where.y;
+			yPoints[4] = where.y + centerOff;
+			yPoints[5] = where.y + centerOff;
 		}
 		
 		return yPoints;
--- Chapter7Stuff-ArrayReview/src/ProbFive.java
+++ Chapter7Stuff-ArrayReview/src/ProbFive.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Point;
 
 
 public class ProbFive {
@@ -6,5 +7,12 @@ public class ProbFive {
 	{
 		Canvas microPaint = new Canvas("Funky Shapes", 1000, 1000, Color.CYAN);
 		microPaint.setVisible(true);
+		
+		microPaint.setInkColor(Color.BLACK);
+		Hexagon theShape = new Hexagon(microPaint, new Point(25, 25), 10, false);
+		Hexagon theOther = new Hexagon(microPaint, new Point(50, 50), 10, true);
+		
+		theShape.moveTo(new Point(200, 500));
+		theOther.moveTo(new Point(200, 500));
 	}
 }
