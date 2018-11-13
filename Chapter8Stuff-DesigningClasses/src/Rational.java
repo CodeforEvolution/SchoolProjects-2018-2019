ac9e1d459b3bc3743df7d5edce23f4d643ad8564
CodeforEvolution <secundaja@gmail.com>
2018-11-13 13:40:09 -0600

    Rational Class Work, Finished

--- Chapter8Stuff-DesigningClasses/src/Rational.java
+++ Chapter8Stuff-DesigningClasses/src/Rational.java
@@ -2,7 +2,7 @@
 public class Rational {
 	private int numerator;
 	private int denominator;
-	private static int operationNumber = 0;
+	public static int operationNumber = 0;
 	
 	public Rational(int num, int den)
 	{
@@ -22,8 +22,10 @@ public class Rational {
 	
 	private void reduce()
 	{
-		numerator /= numerator;
-		denominator /= numerator;
+		int great = gcd(numerator, denominator);
+		
+		numerator /= great;
+		denominator /= great;
 	}
 	
 	public int getNumerator()
@@ -38,22 +40,32 @@ public class Rational {
 	
 	public Rational multiply(Rational otherFrac)
 	{
+		operationNumber++;
 		return new Rational(this.numerator * otherFrac.numerator, this.denominator * otherFrac.denominator);
 	}
 	
 	public Rational divide(Rational otherFrac)
 	{
+		operationNumber++;
 		return new Rational(this.numerator * otherFrac.denominator, this.denominator * otherFrac.numerator);
 	}
 	
 	public Rational add(Rational otherFrac)
 	{
+		int numNew = (this.numerator * otherFrac.denominator) + (otherFrac.numerator * this.denominator);
+		int denNew = this.denominator * otherFrac.denominator;
 		
+		operationNumber++;
+		return new Rational(numNew, denNew);
 	}
 	
 	public Rational subtract(Rational otherFrac)
 	{
+		int numNew = (this.numerator * otherFrac.denominator) - (otherFrac.numerator * this.denominator);
+		int denNew = this.denominator * otherFrac.denominator;
 		
+		operationNumber++;
+		return new Rational(numNew, denNew);
 	}
 	
 	public boolean equals(Object frac2)
@@ -82,11 +94,41 @@ public class Rational {
 	
 	public Rational reciprocal()
 	{
+		int numNew = this.denominator;
+		int denNew = this.numerator;
 		
+		int divisor = gcd(numNew, denNew);
+		
+		return new Rational(numNew / divisor, denNew / divisor);
 	}
 	
 	private int gcd(int first, int second)
 	{
+		int gcd = 0;
+		int greater = 0;
 		
+		if (first < second)
+		{
+			greater = second;
+		}
+		else if (first > second)
+		{
+			greater = first;
+		}
+		else
+		{
+			//Optimize
+			return first;
+		}
+
+		for (int i = 1; i <= greater; i++)
+		{
+			if (first % i == 0 && second % i == 0)
+			{
+				gcd = i;
+			}
+		}
+		
+		return gcd;
 	}
 }
---
+++ Chapter8Stuff-DesigningClasses/src/RationalTester.java
@@ -0,0 +1,24 @@
+
+public class RationalTester {
+	public static void main(String[] args)
+	{
+		Rational f1 = new Rational(12, 16);
+		Rational f2 = new Rational(2, 6);
+		
+		System.out.println("First fraction : " + f1);
+		System.out.println("Second fraction : " + f2);
+		
+		Rational f4 = f1.add(f2);
+		Rational f5 = f1.subtract(f2);
+		Rational f6 = f1.multiply(f2);
+		Rational f7 = f1.divide(f2);
+		
+		System.out.println("f1 + f2: " + f4);
+		System.out.println("f1 - f2: " + f5);
+		System.out.println("f1 * f2: " + f6);
+		System.out.println("f1 / f2: " + f7);
+		
+		System.out.println("Total multiply ops: " + Rational.operationNumber);
+		System.out.println("f1 equals f2? : " + f1.equals(f2));
+	}
+}
