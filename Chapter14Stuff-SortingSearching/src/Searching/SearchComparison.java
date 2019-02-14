926251f6d0c471bb07268ac20e82cfaab27f76c2
CodeforEvolution <secundaja@gmail.com>
2019-02-14 13:39:14 -0600

    Reorganized search code, binary search works

--- Chapter14Stuff-SortingSearching/src/Searching/SearchComparison.java
+++ Chapter14Stuff-SortingSearching/src/Searching/SearchComparison.java
@@ -1,32 +1,30 @@
 package Searching;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class SearchComparison {
+	public static int compares = 0;
+	
 	public static void main(String[] args)
 	{
 		Random r = new Random();
 		Scanner in = new Scanner(System.in);
-		int[] myArray = new int[100];
+		int[] myArray = new int[1000];
 		boolean ok;
 		int search;
 		int foundIndex;
-		int compares;
-		BigDecimal startTime;
-		BigDecimal endTime;
+		double startTime;
+		double endTime;
 		
 		for (int i = 0; i < myArray.length; i++)
 		{
 			myArray[i] = r.nextInt(100) + 1;
 		}
 		
-		System.out.println("The Array: " + Arrays.toString(myArray));
+		System.out.println("The Original Array: " + Arrays.toString(myArray));
 		
-		// Unsorted array
-		System.out.println("\nUnsorted time!\n");
 		System.out.println("Why hello there!");
 		
 		ok = false;
@@ -40,33 +38,29 @@ public class SearchComparison {
 			else
 				ok = true;
 		} while (ok == false);
+		in.close();
 		System.out.println();
 		
-		compares = 0;
-		foundIndex = -1;
-		startTime = new BigDecimal(System.currentTimeMillis());
-		for (int i = 0; i < myArray.length; i++)
-		{
-			compares++;
-			if (search == myArray[i])
-			{
-				foundIndex = i;
-				break;
-			}
-		}
-		endTime = new BigDecimal(System.currentTimeMillis());
+		// The Linear Search with unsorted array
+		System.out.println("Unsorted time!\n");
+		
+		startTime = System.currentTimeMillis();
+		
+		foundIndex = linearSearch(myArray, search);
+		
+		endTime = System.currentTimeMillis();
 		
 		if (foundIndex != -1)
 		{
 			System.out.println("Found " + search + " at index " + foundIndex);
-			System.out.println("Search took " + endTime.subtract(startTime) + " ms and " + compares + " comparisons!");
+			System.out.println("Search took " + (endTime - startTime) + " ms and " + compares + " comparisons!");
 		}
 		else
 		{
 			System.out.println("Could not find " + search);
 		}
 		
-		// Sorted array
+		// Sorted array with binary search
 		
 		System.out.println("\nSorted time!\n");
 		
@@ -75,28 +69,66 @@ public class SearchComparison {
 		
 		System.out.println("\nLet's search again, but binary!");
 		
-		compares = 0;
-		foundIndex = -1;
-		startTime = new BigDecimal(System.currentTimeMillis());
-		for (int i = 0; i < myArray.length; i++)
-		{
-			compares++;
-			if (search == myArray[i])
-			{
-				foundIndex = i;
-				break;
-			}
-		}
-		endTime = new BigDecimal(System.currentTimeMillis());
+		startTime = System.currentTimeMillis();
+		
+		foundIndex = binarySearch(myArray, search);
+		
+		endTime = System.currentTimeMillis();
 		
 		if (foundIndex != -1)
 		{
 			System.out.println("Found " + search + " at index " + foundIndex);
-			System.out.println("Search took " + endTime.subtract(startTime) + " ms and " + compares + " comparisons!");
+			System.out.println("Search took " + (endTime - startTime) + " ms and " + compares + " comparisons!");
 		}
 		else
 		{
 			System.out.println("Could not find " + search);
 		}
+	}
+	
+	public static int linearSearch(int[] arrayToSearch, int toSearchFor)
+	{
+		int foundIndex = -1;
+		compares = 0;
+		
+		for (int i = 0; i < arrayToSearch.length; i++)
+		{
+			compares++;
+			if (toSearchFor == arrayToSearch[i])
+			{
+				foundIndex = i;
+				break;
+			}
+		}
+		
+		return foundIndex;
+	}
+	
+	public static int binarySearch(int[] arrayToSearch, int toSearchFor)
+	{
+		compares = 0;
+		return binarySearch(arrayToSearch, toSearchFor, arrayToSearch.length - 1, 0);
+	}
+	
+	private static int binarySearch(int[] array, int search, int high, int low)
+	{
+		compares++;
+		int mid = (high + low) / 2;
+		
+		if (low > high)
+		{
+			return -1;
+		}
+		else if (array[mid] > search)
+		{
+			return binarySearch(array, search, mid - 1, low);
+		}
+		else if (array[mid] < search)
+		{
+
+			return binarySearch(array, search, high, mid + 1);
+		}
+		
+		return mid;
 	}
 }
