4d82e1690589f77e5add1a6ee44b2c295c2d6557
CodeforEvolution <secundaja@gmail.com>
2019-02-15 13:39:27 -0600

    Start work with math tutor program

---
+++ Chapter14Stuff-SortingSearching/src/Games/MathTutor.java
@@ -0,0 +1,46 @@
+package Games;
+
+public class MathTutor {
+	public enum GameMode {ADD, SUB, MD, ALL};
+	
+	public static void main(String[] args)
+	{
+		int success = 0;
+		GameMode mode = GameMode.ADD;
+		int tries = 3;
+		boolean again = false;
+		
+		System.out.println("Welcome!");
+		printRules();
+		
+		do
+		{
+
+		} while (again == true);
+	}
+	
+	public static void printRules()
+	{
+		System.out.println("I'm Toot, your personal tutor!");
+	}
+	
+	public static boolean level1Ques()
+	{
+		
+	}
+	
+	public static boolean level2Ques()
+	{
+		
+	}
+	
+	public static boolean level3Ques()
+	{
+		
+	}
+	
+	public static boolean level4Ques()
+	{
+		
+	}
+}
