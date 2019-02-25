f5f21ddd75ec6e28a1f2df72f8edfb5b93a0d379
CodeforEvolution <secundaja@gmail.com>
2019-02-25 13:40:05 -0600

    More march madness work, almost done creating brackets and printing info

--- Chapter14Stuff-SortingSearching/src/Games/MadMarchSim.java
+++ Chapter14Stuff-SortingSearching/src/Games/MadMarchSim.java
@@ -9,7 +9,7 @@ import Games.Team.Region;
 
 public class MadMarchSim {
 	public static Random rEngine = new Random();
-	public static int REGION_COUNT = Region.values().length;
+	public static int REGION_COUNT = Region.values().length - 1;
 
 	public static void main(String[] args) throws Exception {
 		ArrayList<Team> teams = new ArrayList<Team>();
@@ -26,16 +26,35 @@ public class MadMarchSim {
 		inEngine.close();
 
 		// Fill team array with 16 teams for each of the 4 regions
-		for (Region region : Region.values()) {
-			for (int seed = 1; seed <= teams.size() / 4; seed++) {
+		for (Region region : Region.values())
+		{
+			for (int seed = 1; (seed - 1) < teamNames.size() / REGION_COUNT / 2; seed++)
+			{
 				teams.add(new Team(teamNames.get(region.getValue() * seed), region, seed));
 			}
 		}
 
-		for (int i = 0; i < teams.size() / REGION_COUNT / 2; i++) {
+		Region currRegion = Region.UNKNOWN;
+		for (int r = 1; r < REGION_COUNT + 1; r++)
+		{
+			currRegion = Region.enumForValue(r);
+			for (int i = 1; i < ((teams.size() / REGION_COUNT) / 2) + 1; i++)
+			{
+				Team destToWin = findTeam(teams, currRegion, i);
+				Team destToLose = findTeam(teams, currRegion, (teams.size() / REGION_COUNT) + 1 - i);
+
+
+				Team winner = randomWinner(destToWin, destToLose);
+
+				System.out.println("\nNow for the " + destToWin.getSeed() + " seed and " + destToLose.getSeed() + " seed:");
+				System.out.println("It's " + destToWin.getName() + " versus " + destToLose.getName() + " in the " + currRegion + " Region!");
+				System.out.println("The winner is..." + winner.getName() + " !");
+
+				winners.add(winner);
+			}
 		}
 
-		System.out.println(teamNames);
+		System.out.println(winners);
 	}
 
 	public static Team findTeam(ArrayList<Team> teams, Region theRegion, int seed) {
@@ -46,6 +65,33 @@ public class MadMarchSim {
 			if (currTeam.getRegion() == theRegion && currTeam.getSeed() == seed)
 				return currTeam;
 		}
+		return null;
+	}
+
+	public static Team randomWinner(Team a, Team b) {
+		if (a.compareTo(b) < 0)
+		{
+			if (rEngine.nextInt(20) > 5)
+			{
+				return a;
+			}
+			else
+			{
+				return b;
+			}
+		}
+		else if (b.compareTo(a) > 0)
+		{
+			if (rEngine.nextInt(20) > 5)
+			{
+				return b;
+			}
+			else
+			{
+				return a;
+			}
+		}
+
 		return null;
 	}
 }
--- Chapter14Stuff-SortingSearching/src/Games/Team.java
+++ Chapter14Stuff-SortingSearching/src/Games/Team.java
@@ -3,47 +3,83 @@ package Games;
 public class Team implements Comparable<Team> {
 	public enum Region
 	{
-		NORTH(1), SOUTH(2), MIDWEST(3), EAST(4);
-						
+		NORTH(1), SOUTH(2), MIDWEST(3), EAST(4), UNKNOWN(5);
+
 		private int internalRep;
-		
+
 		Region(int internal)
 		{
 			internalRep = internal;
 		}
-		
+
 		public int getValue()
 		{
 			return internalRep;
 		}
+
+		public static Region enumForValue(int value)
+		{
+			switch (value)
+			{
+				case 1:
+					return NORTH;
+				case 2:
+					return SOUTH;
+				case 3:
+					return MIDWEST;
+				case 4:
+					return EAST;
+				default:
+					return UNKNOWN;
+			}
+		}
+
+		public String toString()
+		{
+			switch (internalRep)
+			{
+				case 1:
+					return "North";
+				case 2:
+					return "South";
+				case 3:
+					return "Midwest";
+				case 4:
+					return "East";
+				case 5:
+					return "Unknown";
+				default:
+					return "Error";
+			}
+		}
 	};
-	
+
 	private String fName;
 	private Region fRegion;
 	private Integer fSeed;
-	
+
 	public Team(String name, Region startRegion, int seed)
 	{
 		fName = name;
 		fRegion = startRegion;
 		fSeed = seed;
 	}
-	
+
 	public String getName()
 	{
 		return fName;
 	}
-	
+
 	public Region getRegion()
 	{
 		return fRegion;
 	}
-	
+
 	public Integer getSeed()
 	{
 		return fSeed;
 	}
-	
+
 	public int compareTo(Team other)
 	{
 		if (this.getSeed() < other.getSeed())
@@ -59,18 +95,18 @@ public class Team implements Comparable<Team> {
 			return 0;
 		}
 	}
-	
+
 	public String toString()
 	{
 		String out = "";
-		
+
 		out += "Team: " + fName;
 		out += "\n\tRegion: " + regionToString();
 		out += "\n\tSeed: " + fSeed;
-		
+
 		return out;
 	}
-	
+
 	private String regionToString()
 	{
 		switch (fRegion)
