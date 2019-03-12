2dbae2e0636a795cdc8c59dbf0ec1395f7cafcda
CodeforEvolution <secundaja@gmail.com>
2019-03-12 13:38:00 -0500

    Start Elevens Lab! Finish Act 1 and 2.

---
+++ Labs/src/Elevens/CardTester.java
@@ -0,0 +1,19 @@
+package Elevens;
+
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** COMPLETED IN ACTIVITY 1 *** */
+
+		System.out.println(new Card("Jack", "Diamonds", -50));
+		System.out.println(new Card("Jack", "Diamonds", -50));
+		System.out.println(new Card("Queen", "Spades", 30));
+	}
+}
---
+++ Labs/src/Elevens/DeckTester.java
@@ -0,0 +1,24 @@
+package Elevens;
+
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** COMPLETED IN ACTIVITY 2 *** */
+
+		String[] testR = {"Egg", "Bacon", "Cheese"};
+		String[] testS = {"Yes!", "No!", "Maybe :D"};
+		int[] testPA = {100, 4, 2, 3};
+		int[] testPB = {50, -3, 2, 40};
+
+		System.out.println(new Deck(testR, testS, testPA));
+		System.out.println(new Deck(testS, testR, testPA));
+		System.out.println(new Deck(testR, testS, testPB));
+	}
+}
